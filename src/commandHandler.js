5bfce625e2fef54917891d610049eabd50d9582c
iudizm <iudizm@gmail.com>
2021-04-19 21:16:31 -0300

    ✨Adiciona funcionalidade de busca por GIFs issue #5

--- src/commandHandler.js
+++ src/commandHandler.js
@@ -1,24 +1,29 @@
 const fetch = require('node-fetch');
 require('dotenv').config();
 
-module.exports = async function(message) {
+module.exports = async function (message) {
     console.log(`message: ${message.content}'`);
-    if (trigger_words.includes((message.content).toUpperCase())) {
+    let messageParts = message.content.split(' ');
+    if (trigger_words.includes((messageParts[0]).toUpperCase())) {
         message.channel.send('opa!');
         message.react('🎅');
     }
-    if ((message.content).toLowerCase() === "?nick") {
+    if ((messageParts[0]).toLowerCase() === "?nick") {
         message.react('👌');
         const i = Math.floor(Math.random() * randomlyGeneratedNicknames.length);
         message.channel.send(randomlyGeneratedNicknames[i])
     }
-    if ((message.content).toLowerCase() === '?foto') {
+    if ((messageParts[0]).toLowerCase() === '?foto') {
         message.react('👌');
         message.channel.send(message.author.displayAvatarURL());
     }
-    if ((message.content).toLowerCase() === '?gif') {
+    if ((messageParts[0]).toLowerCase() === '?gif') {
         message.react('👌');
-        let tenorApiUrl = `https://g.tenor.com/v1/search?q=santa&key=${process.env.TENOR_API_TOKEN}`;
+        let searchParameters = "marijuana";
+        if (messageParts.length > 1) {
+            searchParameters = messageParts.slice(1, messageParts.length).join(' ');
+        };
+        let tenorApiUrl = `https://g.tenor.com/v1/search?q=${searchParameters}&key=${process.env.TENOR_API_TOKEN}&contentfilter=off`;
         let response = await fetch(tenorApiUrl);
         let json = await response.json();
         let i = Math.floor(Math.random() * json.results.length);
